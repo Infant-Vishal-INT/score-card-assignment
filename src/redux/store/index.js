8f1dbb4b5ebfaee9cfe0993f6ab02e7fe0fd2c17
Infant Vishal <infant.vishal@indusnet.co.in>
2022-10-31 12:51:39 +0530

    removed unwanted code

--- src/redux/store/index.js
+++ src/redux/store/index.js
@@ -12,15 +12,9 @@ const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 const configureStore = () => {
   const store = createStore(persistedReducer);
-
   const persistor = persistStore(store);
 
   return { store, persistor };
 };
 
 export default configureStore;
-
-// export const store = createStore(
-//   rootReducer,
-//   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-// );
